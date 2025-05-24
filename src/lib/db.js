e97ad0ca903dc3c2a7bfd3189edea53b442e57a9
alfian yuanata <alfianyuanata@gmail.com>
2025-05-24 17:51:20 +0700

    refactor db.js so use env for host

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 
 // Database connection setup
 export const db = mysql.createPool({
-  host: 'localhost',  
+  host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER, 
   password: process.env.MYSQL_PASSWORD, 
   database: process.env.MYSQL_DATABASE, 
